61e1688f1bb742b553b25efb7f0353871ce82723
taedi <mail@taedi.net>
2021-11-18 15:32:42 +0900

    토스트 알림창 추가
    - toastAlert 메서드로 실행

---
+++ src/main/webapp/resources/js/common/toast-alert.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const toastStyle = `
+        position: fixed;
+        bottom: 30px;
+        left: 50%;
+        padding: 15px 20px;
+        transform: translate(-50%, 10px);
+        border-radius: 30px;
+        overflow: hidden;
+        font-size: .8rem;
+        opacity: 0;
+        visibility: hidden;
+        transition: opacity .5s, visibility .5s, transform .5s;
+        background: rgba(0, 0, 0, .35);
+        color: #fff;
+        z-index: 10000;`;
+
+const toastElem = document.createElement("div");
+toastElem.id = "toast";
+toastElem.style = toastStyle;
+document.querySelector("body").appendChild(toastElem);
+
+let toastTimeout;
+
+function toastAlert(string) {
+
+    clearTimeout(toastTimeout);
+
+    toastElem.innerText = string;
+
+    toastElem.style.opacity = 1;
+    toastElem.style.visibility = "visible";
+    toastElem.style.transform = "translate(-50%, 0)";
+
+    toastTimeout = window.setTimeout(() => {
+        toastElem.style.opacity = 0;
+        toastElem.style.visibility = "hidden";
+        toastElem.style.transform = "translate(-50%, 10px)";
+    }, 2000);
+}
