590f4cdc902704cc7f6c0455a40f3a1600d99ff5
seunga <tmddk6522@naver.com>
2021-11-30 18:20:43 +0900

    주소추가 스크립트 생성
    - 다음 우편번호 서비스 API적용
    - update시 필요한 주소정보 추가

--- src/main/webapp/resources/js/order/add-address.js
+++ src/main/webapp/resources/js/order/add-address.js
@@ -1,19 +1,4 @@
-function address(){
-	
-	
-	
-var add = 	`  
-    <input type="text" id="sample4_postcode" placeholder="우편번호" class="postal_code">
-    <input type="button" onclick="sample4_execDaumPostcode()" value="우편번호 찾기"><br>
-    <input type="text" id="sample4_roadAddress" placeholder="도로명주소" class="road_name_address"><br>
-    <input type="hidden" id="sample4_jibunAddress" placeholder="지번주소">
-    <span id="guide" style="color:#999;display:none"></span>
-    <input type="text" id="sample4_detailAddress" placeholder="상세주소" class="detailed_address">
-    <input type="hidden" id="sample4_extraAddress" placeholder="참고항목">
-    
-    <script src="//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js"></script>
-    <script>
-        //본 예제에서는 도로명 주소 표기 방식에 대한 법령에 따라, 내려오는 데이터를 조합하여 올바른 주소를 구성하는 방법을 설명합니다.
+ //본 예제에서는 도로명 주소 표기 방식에 대한 법령에 따라, 내려오는 데이터를 조합하여 올바른 주소를 구성하는 방법을 설명합니다.
         function sample4_execDaumPostcode() {
             new daum.Postcode({
                 oncomplete: function(data) {
@@ -68,16 +53,3 @@ var add = 	`
                 }
             }).open();
         }
-    </script>
-    `;
-    
-    
-    newModal(add);
-    
-    
-    
-    
-    
-    
-    
-}
