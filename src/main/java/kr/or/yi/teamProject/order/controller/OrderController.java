b1dfd12d436862d7b52420ddeac9b0811acf5a05
seunga <tmddk6522@naver.com>
2021-12-01 14:03:29 +0900

    결제페이지로 넘기는 ajax 결과값 추가
    - orderUpdate에서 orderNo넘겨주는 로직 수정

--- src/main/java/kr/or/yi/teamProject/order/controller/OrderController.java
+++ src/main/java/kr/or/yi/teamProject/order/controller/OrderController.java
@@ -24,6 +24,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import kr.or.yi.teamProject.order.dto.Order;
 import kr.or.yi.teamProject.order.dto.OrderDetail;
 import kr.or.yi.teamProject.order.dto.OrderItem;
+import kr.or.yi.teamProject.order.mapper.OrderDetailMapper;
 import kr.or.yi.teamProject.order.mapper.OrderMapper;
 import kr.or.yi.teamProject.order.service.OrderDetailService;
 import kr.or.yi.teamProject.order.service.OrderItemService;
@@ -37,149 +38,167 @@ import kr.or.yi.teamProject.security.dto.CustomUser;
 import kr.or.yi.teamProject.user.dto.Member;
 import lombok.extern.slf4j.Slf4j;
 
-
 //OrderController관련컨트롤러
 @Slf4j
 @Controller
 public class OrderController {
-	
+
 	@Autowired
 	OrderItemService orderItemService;
-	
+
 	@Autowired
 	OrderDetailService orderDetailService;
 	
 	@Autowired
+	OrderDetailMapper orderDetailMapper;
+
+	@Autowired
 	OrderService orderService;
-	
+
 	@Autowired
 	OrderMapper orderMapper;
-	
+
 	@Autowired
 	OptionMapper optionMapper;
+
+	// 주문대기열 추가
+	@PostMapping("/order")
+	@ResponseBody
+	public String insertOrder(@RequestBody List<Map<String, Object>> list, Model model, Authentication authentication) {
+
+		// 로그인 정보 가져오기
+		CustomUser user = (CustomUser) authentication.getPrincipal();
+		Member member = user.getMember();
+
+		log.info("=====================insertOrder========================");
+		log.info(list.toString());
+
+		// 옵션 유효성 검사
+		// 재고 확인
+		
+
+		// 주문번호 난수 생성
+		SecureRandom rnd = null;
+		try {
+			rnd = SecureRandom.getInstance("SHA1PRNG");
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		int rndnum = rnd.nextInt(100000);
+
+		String now = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
+		long parseNow = Long.parseLong(now);
+		System.out.println(now);
+		long orderno = (parseNow * 100000) + rndnum;
+
+		System.out.println("==============orderno==============" + orderno);
+
+		// order 객체생성
+		Order order = new Order();
+		order.setOrderNo(orderno);
+		order.setMember(member);
+
+		// 해쉬맵 리스트를 풀어서 orderItems에 담기
+		List<OrderItem> orderItems = new ArrayList<>();
+
+		for (Map<String, Object> m : list) {
+
+			log.info("Long.parseLong(m.get(\"optionNo\").toString())" + Long.parseLong(m.get("optionNo").toString()));
+
+			OrderItem orderItem = new OrderItem();
+			Option option = optionMapper.selectOptionDetail(Long.parseLong(m.get("optionNo").toString()));
+
+			log.info("==============option===============" + option);
+
+			orderItem.setOption(option);
+			orderItem.setOrder(order);
 	
+			int quantity = Integer.parseInt(m.get("quantity").toString());
+			
+			for (int i = 0; i < Integer.parseInt(m.get("quantity").toString()); i++) {
+				orderItems.add(orderItem);
+			}
+			
+			
+////			재고확인
+//			Option stockCk = optionMapper.selectOption(option);
+//			int stock = stockCk.getStock();
+//			if(quantity<stock) {
+//				
+//			}
+			
+		}
 
-    // 주문 조회	old
-  @GetMapping("/order")
-  public String order(@RequestParam("orderNo")Long orderNo, Model model) {
-	  
-	  System.out.println("=====================================================");
-	  	  	  
-	  Order orderDetail = orderDetailService.selectDetailOrder(Order.builder().orderNo(orderNo).build());
-				  
-	  model.addAttribute("orderDetail",orderDetail);
-	  log.info(orderDetail.toString());
-	  	  
-  	  return "/order/order";
-  }
+		log.info("============객체확인===========" + order.toString() + orderItems.toString());
 
-  
-  
-  //주문대기열 추가
-  @PostMapping("/order")
-  @ResponseBody
-  public String insertOrder(@RequestBody List<Map<String, Object>> list, Model model, Authentication authentication) {
-	  
-	  // 로그인 정보 가져오기
-      CustomUser user = (CustomUser) authentication.getPrincipal();
-      Member member = user.getMember();
-	  
-	
-	  log.info("=====================insertOrder========================");
-	  log.info(list.toString());
+		// 주문대기열 추가
+		int res = orderService.createOrderMember(order, orderItems);
+		if (res == 1) {
+			return String.valueOf(orderno);
+		}
+		return "";
 
-	  //옵션 유효성 검사
-	  //재고 확인
+	}
 
-	  
-	  //주문번호 난수 생성
-	  SecureRandom rnd = null;
-      try {
-          rnd = SecureRandom.getInstance("SHA1PRNG");
-      } catch (NoSuchAlgorithmException e) {
-          e.printStackTrace();
-      }
-	  int rndnum = rnd.nextInt(100000);
-	  
-	  String now = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
-	  long parseNow = Long.parseLong(now);
-	  System.out.println(now);
-	  long orderno =  (parseNow * 100000) + rndnum;
-	  
-	  System.out.println("==============orderno=============="+orderno);
+	// 주문 페이지
+	@GetMapping("/order")
+	public String order(@RequestParam("orderNo") Long orderNo, Model model) {
 
-	  
-	  //order 객체생성
-	  Order order = new Order() ;	 
-	  order.setOrderNo(orderno);
-	  order.setMember(member);
+		log.info("========================order=============================");
 
-	  
-	   
-	   // 해쉬맵 리스트를 풀어서 orderItems에 담기
-	   List<OrderItem> orderItems =new ArrayList<>();
-	   
-	   for(Map<String, Object> m : list) {
+		Order orderDetail = orderDetailService.selectDetailOrder(Order.builder().orderNo(orderNo).build());
 
-		   log.info("Long.parseLong(m.get(\"optionNo\").toString())"+Long.parseLong(m.get("optionNo").toString()));
-	      
-		   
-	     OrderItem orderItem = new OrderItem();	      
-	     Option option =  optionMapper.selectOptionDetail(Long.parseLong(m.get("optionNo").toString()));
+		model.addAttribute("orderDetail", orderDetail);
+		log.info(orderDetail.toString());
+
+		return "/order/order";
+	}
+
+	// 주문 update
+	@PostMapping("/orderUpdate")
+	@ResponseBody
+	public String orderUpdate(@RequestBody Map<String, Object> updateData, Model model) {
+
+		log.info("======================orderUpdate===================");
+		log.info(updateData.toString());
+
+		//객체생성
+	     Order order = new Order();
 	     
-	     log.info("==============option==============="+option);
-   
-	      orderItem.setOption(option);
-	      orderItem.setOrder(order);
-	            
-	      for(int i = 0; i<Integer.parseInt(m.get("quantity").toString());i++) {                  
-	         orderItems.add(orderItem);
-	      }
-	      
-	   }	   
+	     order.setAddress1(updateData.get("address1").toString());
+	     order.setAddress2(updateData.get("address2").toString());
+	     order.setZipcode(updateData.get("zipcode").toString());
+	     order.setName(updateData.get("name").toString());
+	     order.setPhone(updateData.get("phone").toString());
+	     order.setOrderNo(Long.parseLong(updateData.get("orderNo").toString()));
+	     
+	     Long orderNo = Long.parseLong(updateData.get("orderNo").toString());
+	     
+	     int res = orderMapper.updateOrder(order);
+	     
+	     log.info("==========result============"+res);   
+	     
+	     if(res == 1 ) {
+	        return String.valueOf(orderNo);
+	     }
+	     
+	     return"";
+	}
 
-	   log.info("============객체확인==========="+order.toString()+orderItems.toString());
-   
-	   //주문대기열 추가
-	   int res = orderService.createOrderMember(order, orderItems);
-	   if(res == 1 ) {
-		   return String.valueOf(orderno);
-	   }
-	   	   
-	   return "";
+	// 마이페이지 주문확인
+//	@PostMapping("/mypageOrder")
+//	public String mypageOrder(@RequestParam("username") String username, Model model) {
+//		
+//		
+//		Member member = Member.builder().username(username).build();
+//	 
+//		log.info(member.toString());
+//		
+//		List<Order> orderDetailSelectList = orderDetailMapper.orderDetailSelectList(member);
+//		
+//		model.addAttribute("orderDetailSelectList",orderDetailSelectList);
+//				
+//		return "/user/mypage";
+//	}
 
-  }
-  
-  
-  
-  //주문 update
-  @PostMapping("/orderUpdate")
-  @ResponseBody
-  public void orderUpdate(@RequestBody Map<String, Object> updateData, Model model) {
-	  
-	  log.info("======================orderUpdate===================");
-	  log.info(updateData.toString());
-	  
-	  //객체생성
-	  Order order = new Order();
-	  
-	  order.setAddress1(updateData.get("address1").toString());
-	  order.setAddress2(updateData.get("address2").toString());
-	  order.setZipcode(updateData.get("zipcode").toString());
-	  order.setName(updateData.get("name").toString());
-	  order.setPhone(updateData.get("phone").toString());
-	  order.setOrderNo(Long.parseLong(updateData.get("orderNo").toString()));
-	  
-	  int res = orderMapper.updateOrder(order);
-	  
-	  log.info("==========result============"+res);	  
-  }
-  
-  
-  
-  
-
-
-	
-	
 }
