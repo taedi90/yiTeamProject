81ae9376913a80b1478a30c4520842857eff7c2f
taedi90 <taedi90@gmail.com>
2021-12-01 19:18:04 +0900

    메일 센더 오류 수정

--- src/main/java/kr/or/yi/teamProject/config/MailConfig.java
+++ src/main/java/kr/or/yi/teamProject/config/MailConfig.java
@@ -30,6 +30,7 @@ public class MailConfig {
         properties.put("mail.smtp.starttls.required", true);
         properties.put("mail.smtp.ssl.trust", true);
         properties.put("mail.debug", true);
+        properties.put("mail.smtp.ssl.protocols", "TLSv1.2");
 
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost(env.getProperty("mailSender.host"));
